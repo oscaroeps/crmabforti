3eaccf0778511feefe39bd453f0d965d6363065f
Oscar Emanuel Pascual Sánchez <42615048+oscaroeps@users.noreply.github.com>
2024-11-04 11:53:37 -0600

    se agregan condiciones en el front para que sea más responsivo

--- client_dev/src/app/components/top/top.component.ts
+++ client_dev/src/app/components/top/top.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { TestService } from 'src/app/services/test.service';
 declare var KTLayoutAside: any;
 declare var KTLayoutHeaderMenu: any;
@@ -15,6 +15,7 @@ export class TopComponent implements OnInit {
   public user: any = {};
   public background = '';
   public showLogout = false; // Propiedad para controlar la visibilidad del botón de cerrar sesión
+  public isSmallScreen: boolean = false; // Nueva propiedad para el tamaño de la pantalla
 
   constructor(
     private _testService: TestService
@@ -22,6 +23,7 @@ export class TopComponent implements OnInit {
     let str_user = localStorage.getItem('user');
     this.user = str_user ? JSON.parse(str_user) : null;
     this.init_config();
+    this.checkScreenSize(); // Verifica el tamaño de la pantalla al cargar
   }
 
   ngOnInit(): void {
@@ -30,6 +32,15 @@ export class TopComponent implements OnInit {
       KTLayoutHeaderMenu.init('kt_header_menu', 'kt_header_menu_wrapper');
       KTLayoutHeaderTopbar.init('kt_header_mobile_topbar_toggle');
     }, 50);
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkScreenSize(); // Llama a este método cuando la ventana se redimensiona
+  }
+
+  private checkScreenSize() {
+    this.isSmallScreen = window.innerWidth <= 991.98;
   }
 
   logout() {
